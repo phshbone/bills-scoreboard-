8e8897b40ac6a7c4a929153b52d4d486bdaec201
phshbone <phshbone@gmail.com>
2026-09-16 14:37:58 -0400

    Stage 11: add Philadelphia team library and refresh-aware app registration

--- app.js
+++ app.js
@@ -9,7 +9,11 @@
         { id: 'jets', name: 'New York Jets', league: 'NFL', sport: 'football', image: 'assets/jets.webp', defaultSelected: true, provider: { sport: 'football', league: 'nfl', team: 'nyj' } },
         { id: 'rangers', name: 'New York Rangers', league: 'NHL', sport: 'hockey', image: 'assets/rangers.webp', defaultSelected: true, provider: { sport: 'hockey', league: 'nhl', team: 'nyr' } },
         { id: 'army', name: 'Army Black Knights', league: 'NCAA', sport: 'football', image: 'assets/army.webp', defaultSelected: true, provider: { sport: 'football', league: 'college-football', team: '349' } },
-        { id: 'fever', name: 'Indiana Fever', league: 'WNBA', sport: 'basketball', image: 'assets/fever.webp', defaultSelected: true, provider: { sport: 'basketball', league: 'wnba', team: 'ind' } }
+        { id: 'fever', name: 'Indiana Fever', league: 'WNBA', sport: 'basketball', image: 'assets/fever.webp', defaultSelected: true, provider: { sport: 'basketball', league: 'wnba', team: 'ind' } },
+        { id: 'eagles', name: 'Philadelphia Eagles', league: 'NFL', sport: 'football', image: 'assets/eagles.webp', defaultSelected: false, provider: { sport: 'football', league: 'nfl', team: 'phi' } },
+        { id: 'phillies', name: 'Philadelphia Phillies', league: 'MLB', sport: 'baseball', image: 'assets/phillies.webp', defaultSelected: false, provider: { sport: 'baseball', league: 'mlb', team: 'phi' } },
+        { id: 'flyers', name: 'Philadelphia Flyers', league: 'NHL', sport: 'hockey', image: 'assets/flyers.webp', defaultSelected: false, provider: { sport: 'hockey', league: 'nhl', team: 'phi' } },
+        { id: 'sixers', name: 'Philadelphia 76ers', league: 'NBA', sport: 'basketball', image: 'assets/sixers.webp', defaultSelected: false, provider: { sport: 'basketball', league: 'nba', team: 'phi' } }
       ]);
 
       const byId = new Map(teamLibrary.map(team => [team.id, team]));
@@ -283,7 +287,9 @@
 
       if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-          navigator.serviceWorker.register('./sw.js').catch(() => {
+          navigator.serviceWorker.register('./sw.js').then(registration => {
+            registration.update().catch(() => {});
+          }).catch(() => {
             // The board remains fully usable online if service-worker registration is unavailable.
           });
         }, { once: true });
