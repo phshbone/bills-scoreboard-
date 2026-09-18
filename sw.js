835fa4cf06370b897756856d2a10dace7e9f4798
phshbone <phshbone@gmail.com>
2026-09-18 17:17:01 -0400

    Stage 11B26: cache remaining team themes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'scoreboard-v11b25-mlb-header-tune-mets';
+const CACHE = 'scoreboard-v11b26-remaining-team-themes';
 const ASSETS = [
   './',
   './index.html',
@@ -26,6 +26,7 @@ const ASSETS = [
   './team-theme-v17.css',
   './team-theme-v18.css',
   './team-theme-v19.css',
+  './team-theme-v20.css',
   './desktop-nav-v11.css',
   './header-plate-v11.css',
   './team-data-v8.js',
